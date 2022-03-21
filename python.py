c91eab3c65d8728d35e3ea48344a36e935782eef
Dong Hwa Nam <100738166+donghwa-nam@users.noreply.github.com>
2022-03-22 00:03:11 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1035,3 +1035,56 @@ QUIZ) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있
 
 # firebat1.damaged(25)
 # firebat1.damaged(25)
+# ---------------------------------------------------
+
+# 다중상속
+
+
+# class Unit:
+#         def __init__(self, name, hp):
+#                 self.name = name # 클래스 안에 들어가있는 name 같은 것을 멤버변수라고 한다
+#                 self.hp = hp
+                
+
+# # 공격 유닛
+
+# class AttackUnit(Unit):
+#         def __init__(self, name, hp, damage):
+#                 Unit.__init__(self, name, hp) # 상속 받은 부분
+#                 self.damage = damage
+
+#         def attack(self, location):
+#                 print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+#                         .format(self.name, location, self.damage)) # 클래스 안에서 메소드 앞에는 항상 self를 쓴다고 알아라
+
+#         def damaged(self, damage):
+#                 print("{0} : {1} 데미지를 입었습니다.".format(self.name,damage))
+#                 self.hp -= damage
+#                 print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#                 if self.hp <= 0:
+#                         print("{0} : 파괴되었습니다.".format(self.name))
+
+# # 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃/ 탱크 등을 수송. 공격 X
+
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#         def __init__(self, flying_speed):
+#             self.flying_speed = flying_speed
+
+#         def fly(self, name, location):
+#                 print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+#                         .format(name, location, self.flying_speed))
+
+# # 공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#         def __init__(self, name, hp, damage, flying_speed):
+#             AttackUnit.__init__(self, name, hp, damage)
+#             Flyable.__init__(self, flying_speed)
+
+# # 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+# ---------------------------------------------------
+
+# 메소드 오버라이딩
+
