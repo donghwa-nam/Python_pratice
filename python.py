80aef336bdcab0b5a46b94187ae7c9b33be93b85
Dong Hwa Nam <100738166+donghwa-nam@users.noreply.github.com>
2022-03-21 23:04:04 +0900

    Update python.py

--- python.py
+++ python.py
@@ -940,10 +940,67 @@ QUIZ) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있
 # attack(tank_name, "1시", tank_damage)
 # attack(tank2_name, "1시", tank2_damage)
 
-class Unit:
-        def __init__(self, name, hp, damage):
-                self.name = name
-                self.hp = hp
-                self.damage = damage
-                print("{0} 유닛이 생성 되었습니다.".format(self.name))
-                print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+# class Unit:
+#         def __init__(self, name, hp, damage):
+#                 self.name = name # 클래스 안에 들어가있는 name 같은 것을 멤버변수라고 한다
+#                 self.hp = hp
+#                 self.damage = damage
+#                 print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#                 print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+# marine1 = Unit("마린", 40, 5)  
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("마린", 40, 5)  
+
+# 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage)) # .으로 멤버변수 접근
+
+# # 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("빼앗은 레이스", 80, 5)
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+#         print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+# ---------------------------------------------------
+
+# 메소드
+
+# class Unit:
+#         def __init__(self, name, hp, damage):
+#                 self.name = name # 클래스 안에 들어가있는 name 같은 것을 멤버변수라고 한다
+#                 self.hp = hp
+#                 self.damage = damage
+#                 print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#                 print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+
+# class AttackUnit:
+#         def __init__(self, name, hp, damage):
+#                 self.name = name # 클래스 안에 들어가있는 name 같은 것을 멤버변수라고 한다
+#                 self.hp = hp
+#                 self.damage = damage
+        
+#         def attack(self, location):
+#                 print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+#                         .format(self.name, location, self.damage)) # 클래스 안에서 메소드 앞에는 항상 self를 쓴다고 알아라
+
+#         def damaged(self, damage):
+#                 print("{0} : {1} 데미지를 입었습니다.".format(self.name,damage))
+#                 self.hp -= damage
+#                 print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#                 if self.hp <= 0:
+#                         print("{0} : 파괴되었습니다.".format(self.name))
+
+# # 파이어뱃 : 공격 유닛, 화염방사기.
+
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+# ---------------------------------------------------
+
+# 상속
+
